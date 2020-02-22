637a93e8846055f3732b3452fc21df6ba503cbfc
Reaperfan <Reaperfanz@yahoo.com>
2020-02-22 22:28:05 +0100

    Add project files.

---
+++ SzakdogaCtrlV2/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace SzakdogaCtrlV2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.timeLbl = new DevExpress.XtraEditors.LabelControl();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.sessionLbl = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl4 = new DevExpress.XtraEditors.LabelControl();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.totalHrLbl = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.SuspendLayout();
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelControl1.Appearance.Options.UseFont = true;
+            this.labelControl1.Location = new System.Drawing.Point(12, 97);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(38, 25);
+            this.labelControl1.TabIndex = 0;
+            this.labelControl1.Text = "Idő:";
+            // 
+            // timeLbl
+            // 
+            this.timeLbl.Appearance.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.timeLbl.Appearance.ForeColor = System.Drawing.Color.Blue;
+            this.timeLbl.Appearance.Options.UseFont = true;
+            this.timeLbl.Appearance.Options.UseForeColor = true;
+            this.timeLbl.Location = new System.Drawing.Point(113, 97);
+            this.timeLbl.Name = "timeLbl";
+            this.timeLbl.Size = new System.Drawing.Size(80, 25);
+            this.timeLbl.TabIndex = 1;
+            this.timeLbl.Text = "00:00:00";
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(12, 208);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(99, 30);
+            this.simpleButton1.TabIndex = 2;
+            this.simpleButton1.Text = "Start";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // sessionLbl
+            // 
+            this.sessionLbl.Appearance.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.sessionLbl.Appearance.ForeColor = System.Drawing.Color.Red;
+            this.sessionLbl.Appearance.Options.UseFont = true;
+            this.sessionLbl.Appearance.Options.UseForeColor = true;
+            this.sessionLbl.Location = new System.Drawing.Point(113, 46);
+            this.sessionLbl.Name = "sessionLbl";
+            this.sessionLbl.Size = new System.Drawing.Size(80, 25);
+            this.sessionLbl.TabIndex = 5;
+            this.sessionLbl.Text = "00:00:00";
+            // 
+            // labelControl4
+            // 
+            this.labelControl4.Appearance.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelControl4.Appearance.Options.UseFont = true;
+            this.labelControl4.Location = new System.Drawing.Point(12, 46);
+            this.labelControl4.Name = "labelControl4";
+            this.labelControl4.Size = new System.Drawing.Size(76, 25);
+            this.labelControl4.TabIndex = 4;
+            this.labelControl4.Text = "Session:";
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Location = new System.Drawing.Point(187, 209);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(96, 29);
+            this.simpleButton2.TabIndex = 6;
+            this.simpleButton2.Text = "Exit";
+            this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // totalHrLbl
+            // 
+            this.totalHrLbl.Appearance.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.totalHrLbl.Appearance.ForeColor = System.Drawing.Color.DarkViolet;
+            this.totalHrLbl.Appearance.Options.UseFont = true;
+            this.totalHrLbl.Appearance.Options.UseForeColor = true;
+            this.totalHrLbl.Location = new System.Drawing.Point(113, 154);
+            this.totalHrLbl.Name = "totalHrLbl";
+            this.totalHrLbl.Size = new System.Drawing.Size(80, 25);
+            this.totalHrLbl.TabIndex = 8;
+            this.totalHrLbl.Text = "00:00:00";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelControl2.Appearance.Options.UseFont = true;
+            this.labelControl2.Location = new System.Drawing.Point(12, 154);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(76, 25);
+            this.labelControl2.TabIndex = 9;
+            this.labelControl2.Text = "Órában:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 247);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.totalHrLbl);
+            this.Controls.Add(this.simpleButton2);
+            this.Controls.Add(this.sessionLbl);
+            this.Controls.Add(this.labelControl4);
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.timeLbl);
+            this.Controls.Add(this.labelControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.LookAndFeel.UseDefaultLookAndFeel = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Szakdoga";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl timeLbl;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.LabelControl sessionLbl;
+        private DevExpress.XtraEditors.LabelControl labelControl4;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private System.Windows.Forms.Timer timer1;
+        private DevExpress.XtraEditors.LabelControl totalHrLbl;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+    }
+}
+
---
+++ SzakdogaCtrlV2/Form1.cs
@@ -0,0 +1,138 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SzakdogaCtrlV2
+{
+    public partial class Form1 : XtraForm
+    {
+        Stopwatch stopwatch;
+        double allsecs;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            stopwatch = new Stopwatch();
+            timer1.Enabled = true;
+            allsecs = LoadAll();
+            TimeSpan ts = TimeSpan.FromSeconds(allsecs);
+
+            // Format and display the TimeSpan value.
+            timeLbl.Text = String.Format("{3} {4} \n{0:00}:{1:00}:{2:00}",
+                 ts.Hours, ts.Minutes, ts.Seconds, ts.Days, "nap");
+            totalHrLbl.Text = String.Format("{0:00} {1}",
+                 ts.TotalHours,"óra");
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            if (simpleButton1.Text == "Start")
+            {
+                stopwatch.Start();
+                simpleButton1.Text = "Stop";
+
+            }
+            else
+            {
+                stopwatch.Stop();
+                simpleButton1.Text = "Start";
+                SaveTime(stopwatch.Elapsed);
+                stopwatch.Reset();
+                double allstop = LoadAll();
+                TimeSpan ts = TimeSpan.FromSeconds(allstop);
+
+                // Format and display the TimeSpan value.
+                timeLbl.Text = String.Format("{3} {4} \n{0:00}:{1:00}:{2:00}",
+                 ts.Hours, ts.Minutes, ts.Seconds, ts.Days, "nap");
+                totalHrLbl.Text = String.Format("{0:00} {1}",
+                 ts.TotalHours, "óra");
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TimeSpan ts = stopwatch.Elapsed;
+
+            // Format and display the TimeSpan value.
+            sessionLbl.Text = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                 ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
+
+        }
+
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SaveTime(TimeSpan secs)
+        {
+            double session = secs.TotalSeconds;
+
+            double allsecs = LoadAll();
+
+            double sumsecs = session + allsecs;
+
+            try
+            {
+                File.WriteAllText("Timespent.txt", sumsecs.ToString());
+            }
+            catch (Exception)
+            {
+
+                XtraMessageBox.Show("Váratlan esemény");
+            }
+        }
+        private double LoadAll()
+        {
+            try
+            {
+                if (File.Exists("Timespent.txt"))
+                {
+                    string[] lines = File.ReadAllLines("Timespent.txt");
+                    return Convert.ToDouble(lines[0]);
+                }
+                else
+                {
+
+                    return 0;
+                }
+            }
+            catch (Exception)
+            {
+                XtraMessageBox.Show("Váratlan hiba, az adatok betöltésekor!");
+                return 0;
+            }
+        }
+        private bool ExitApp()
+        {
+            if (XtraMessageBox.Show("Biztosan kilépsz?", "Kilépés", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                return true;
+                
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (ExitApp())
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+            
+        }
+    }
+}
